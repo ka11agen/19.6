ba6bca30d6445ade46ad5f693191d8d9ec27bc8d
Kapustin Anton <kapustin.anton199712@gmail.com>
2019-05-20 00:28:13 +0700

    Не оптимизированная но рабочая

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.timer4 = new System.Windows.Forms.Timer(this.components);
+            this.timer5 = new System.Windows.Forms.Timer(this.components);
+            this.timer6 = new System.Windows.Forms.Timer(this.components);
+            this.timer7 = new System.Windows.Forms.Timer(this.components);
+            this.timer8 = new System.Windows.Forms.Timer(this.components);
+            this.timer9 = new System.Windows.Forms.Timer(this.components);
+            this.timer10 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(-1, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(801, 451);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 1000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // timer3
+            // 
+            this.timer3.Interval = 1000;
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // timer4
+            // 
+            this.timer4.Interval = 1000;
+            this.timer4.Tick += new System.EventHandler(this.timer4_Tick);
+            // 
+            // timer5
+            // 
+            this.timer5.Interval = 1000;
+            this.timer5.Tick += new System.EventHandler(this.timer5_Tick);
+            // 
+            // timer6
+            // 
+            this.timer6.Interval = 1000;
+            this.timer6.Tick += new System.EventHandler(this.timer6_Tick);
+            // 
+            // timer7
+            // 
+            this.timer7.Interval = 1000;
+            this.timer7.Tick += new System.EventHandler(this.timer7_Tick);
+            // 
+            // timer8
+            // 
+            this.timer8.Interval = 1000;
+            this.timer8.Tick += new System.EventHandler(this.timer8_Tick);
+            // 
+            // timer9
+            // 
+            this.timer9.Interval = 1000;
+            this.timer9.Tick += new System.EventHandler(this.timer9_Tick);
+            // 
+            // timer10
+            // 
+            this.timer10.Interval = 1000;
+            this.timer10.Tick += new System.EventHandler(this.timer10_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Timer timer4;
+        private System.Windows.Forms.Timer timer5;
+        private System.Windows.Forms.Timer timer6;
+        private System.Windows.Forms.Timer timer7;
+        private System.Windows.Forms.Timer timer8;
+        private System.Windows.Forms.Timer timer9;
+        private System.Windows.Forms.Timer timer10;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,261 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        int x;
+        public Form1()
+        {
+            InitializeComponent();
+            Draw_1();
+            Draw_2();
+            Draw_3();
+            Draw_4();
+            Draw_5();
+            Draw_6();
+            Draw_7();
+            Draw_8();
+            Draw_9();
+            Draw_10();
+        }
+        void Draw_1()
+        {
+            Bitmap bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            Graphics graph = Graphics.FromImage(bmp);
+            Pen pen = new Pen(Color.Black);
+            graph.DrawLine(pen, 120, 0, 120, 460);//Стенки
+            graph.DrawLine(pen, 720, 0, 720, 460);
+            graph.DrawEllipse(pen, 120, 350, 100, 100);//Мячик
+            pictureBox1.Image = bmp;
+            timer1.Start();
+            x = 1;
+        }
+        void Draw_2()
+        {
+            Bitmap bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            Graphics graph = Graphics.FromImage(bmp);
+            Pen pen = new Pen(Color.Black);
+            graph.DrawLine(pen, 120, 0, 120, 460);//Стенки
+            graph.DrawLine(pen, 720, 0, 720, 460);
+            graph.DrawEllipse(pen, 220, 350, 100, 100);//Мячик
+            pictureBox1.Image = bmp;
+            timer2.Start();
+            x = 2;
+        }
+        void Draw_3()
+        {
+            Bitmap bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            Graphics graph = Graphics.FromImage(bmp);
+            Pen pen = new Pen(Color.Black);
+            graph.DrawLine(pen, 120, 0, 120, 460);//Стенки
+            graph.DrawLine(pen, 720, 0, 720, 460);
+            graph.DrawEllipse(pen, 320, 350, 100, 100);//Мячик
+            pictureBox1.Image = bmp;
+            timer3.Start();
+            x = 3;
+        }
+        void Draw_4()
+        {
+            Bitmap bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            Graphics graph = Graphics.FromImage(bmp);
+            Pen pen = new Pen(Color.Black);
+            graph.DrawLine(pen, 120, 0, 120, 460);//Стенки
+            graph.DrawLine(pen, 720, 0, 720, 460);
+            graph.DrawEllipse(pen, 420, 350, 100, 100);//Мячик
+            pictureBox1.Image = bmp;
+            timer4.Start();
+            x = 4;
+        }
+        void Draw_5()
+        {
+            Bitmap bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            Graphics graph = Graphics.FromImage(bmp);
+            Pen pen = new Pen(Color.Black);
+            graph.DrawLine(pen, 120, 0, 120, 460);//Стенки
+            graph.DrawLine(pen, 720, 0, 720, 460);
+            graph.DrawEllipse(pen, 520, 350, 100, 100);//Мячик
+            pictureBox1.Image = bmp;
+            timer5.Start();
+            x = 5;
+        }
+        void Draw_6()
+        {
+            Bitmap bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            Graphics graph = Graphics.FromImage(bmp);
+            Pen pen = new Pen(Color.Black);
+            graph.DrawLine(pen, 120, 0, 120, 460);//Стенки
+            graph.DrawLine(pen, 720, 0, 720, 460);
+            graph.DrawEllipse(pen, 620, 350, 100, 100);//Мячик
+            pictureBox1.Image = bmp;
+            timer6.Start();
+            x = 6;
+        }
+        void Draw_10()
+        {
+            Bitmap bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            Graphics graph = Graphics.FromImage(bmp);
+            Pen pen = new Pen(Color.Black);
+            graph.DrawLine(pen, 120, 0, 120, 460);//Стенки
+            graph.DrawLine(pen, 720, 0, 720, 460);
+            graph.DrawEllipse(pen, 220, 350, 100, 100);//Мячик
+            pictureBox1.Image = bmp;
+            timer10.Start();
+            x = 10;
+        }
+        void Draw_9()
+        {
+            Bitmap bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            Graphics graph = Graphics.FromImage(bmp);
+            Pen pen = new Pen(Color.Black);
+            graph.DrawLine(pen, 120, 0, 120, 460);//Стенки
+            graph.DrawLine(pen, 720, 0, 720, 460);
+            graph.DrawEllipse(pen, 320, 350, 100, 100);//Мячик
+            pictureBox1.Image = bmp;
+            timer9.Start();
+            x = 9;
+        }
+        void Draw_8()
+        {
+            Bitmap bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            Graphics graph = Graphics.FromImage(bmp);
+            Pen pen = new Pen(Color.Black);
+            graph.DrawLine(pen, 120, 0, 120, 460);//Стенки
+            graph.DrawLine(pen, 720, 0, 720, 460);
+            graph.DrawEllipse(pen, 420, 350, 100, 100);//Мячик
+            pictureBox1.Image = bmp;
+            timer8.Start();
+            x = 8;
+        }
+        void Draw_7()
+        {
+            Bitmap bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            Graphics graph = Graphics.FromImage(bmp);
+            Pen pen = new Pen(Color.Black);
+            graph.DrawLine(pen, 120, 0, 120, 460);//Стенки
+            graph.DrawLine(pen, 720, 0, 720, 460);
+            graph.DrawEllipse(pen, 520, 350, 100, 100);//Мячик
+            pictureBox1.Image = bmp;
+            timer7.Start();
+            x = 7;
+        }
+       
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Draw_2();
+            timer1.Stop();
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            Draw_3();
+            timer2.Stop();
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            Draw_4();
+            timer3.Stop();
+        }
+
+        private void timer4_Tick(object sender, EventArgs e)
+        {
+            Draw_5();
+            timer4.Stop();
+        }
+
+        private void timer5_Tick(object sender, EventArgs e)
+        {
+            Draw_6();
+            timer5.Stop();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            timer2.Stop();
+            timer3.Stop();
+            timer4.Stop();
+            timer5.Stop();
+            timer6.Stop();
+            timer7.Stop();
+            timer8.Stop();
+            timer9.Stop();
+            timer10.Stop();
+            Draw_1();
+            
+        }
+
+        private void timer6_Tick(object sender, EventArgs e)
+        {
+            Draw_7();
+            timer6.Stop();
+        }
+
+        private void timer7_Tick(object sender, EventArgs e)
+        {
+            Draw_8();
+            timer7.Stop();
+        }
+
+        private void timer8_Tick(object sender, EventArgs e)
+        {
+            Draw_9();
+            timer8.Stop();
+        }
+
+        private void timer9_Tick(object sender, EventArgs e)
+        {
+            Draw_10();
+            timer9.Stop();
+        }
+
+        private void timer10_Tick(object sender, EventArgs e)
+        {
+            Draw_1();
+            timer10.Stop();
+        }
+
+       
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            timer1.Stop();
+            timer2.Stop();
+            timer3.Stop();
+            timer4.Stop();
+            timer5.Stop();
+            timer6.Stop();
+            timer7.Stop();
+            timer8.Stop();
+            timer9.Stop();
+            timer10.Stop();
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (x)
+            {
+                case 1: timer1.Start(); break;
+                case 2: timer2.Start(); break;
+                case 3: timer3.Start(); break;
+                case 4: timer4.Start(); break;
+                case 5: timer5.Start(); break;
+                case 6: timer6.Start(); break;
+                case 7: timer7.Start(); break;
+                case 8: timer8.Start(); break;
+                case 9: timer9.Start(); break;
+                case 10: timer10.Start(); break;
+            }  
+        }
+    }
+}
